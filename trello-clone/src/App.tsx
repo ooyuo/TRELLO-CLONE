f9dc263dbb8c666a06451f2d3ae9f90acd8dd50d
ooyuo <doyounjeong0@gmail.com>
2022-08-27 14:06:35 +0900

    styles & placeholder

--- trello-clone/src/App.tsx
+++ trello-clone/src/App.tsx
@@ -1,23 +1,69 @@
 import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  display: flex;
+  max-width: 480px;
+  width: 100%;
+  margin: 0 auto;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
+
+const Boards = styled.div`
+  display: grid;
+  width: 100%;
+  grid-template-columns: repeat(1, 1fr);
+`;
+
+const Board = styled.div`
+  padding: 20px 10px;
+  padding-top: 30px;
+  background-color: ${(props) => props.theme.boardColor};
+  border-radius: 5px;
+  min-height: 200px;
+`;
+
+const Card = styled.div`
+  border-radius: 5px;
+  margin-bottom: 5px;
+  padding: 10px 10px;
+  background-color: ${(props) => props.theme.cardColor};
+`;
+
+const toDos = ["a", "b", "c", "d", "e", "f"];
 
 function App() {
   const onDragEnd = () => {};
   return (
     <DragDropContext onDragEnd={onDragEnd}>
-      <div>
+      <Wrapper>
+        <Boards>
         <Droppable droppableId="one">
-          {() => (
-            <ul>
-              <Draggable draggableId="first" index={0}>
-                {() => <li>One</li>}
-              </Draggable>
-              <Draggable draggableId="second" index={1}>
-                {() => <li>Two</li>}
-              </Draggable>
-            </ul>
+          {(magic) => (
+            <Boa  rd ref={magic.innerRef} {...magic.droppableProps}>
+              {toDos.map((toDo, index) => (
+                <Draggable draggableId={toDo} index={index}>
+                  {(magic) => (
+                    <Card
+                      ref={magic.innerRef}
+                      {...magic.dragHandleProps}
+                      {...magic.draggableProps}
+                    >
+                      {toDo}
+                    </Card>
+                  )}
+                </Draggable>
+              ))}
+              {magic.placeholder}
+            </Boa>
           )}
         </Droppable>
-      </div>
+        </Boards>
+      </Wrapper>
     </DragDropContext>
   )
 }
+
+export default App;
